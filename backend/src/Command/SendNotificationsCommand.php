f89c8a7dd916c42606451db968502715801fc669
Q-digital <no-reply@q-digital.org>
2024-12-09 19:30:56 +0400

    tmp

--- backend/src/Command/SendNotificationsCommand.php
+++ backend/src/Command/SendNotificationsCommand.php
@@ -4,6 +4,7 @@ namespace App\Command;
 
 use App\Entity\TaskNotification;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -16,8 +17,9 @@ class SendNotificationsCommand extends Command
     private EntityManagerInterface $entityManager;
     private MessageBusInterface $bus;
 
-    public function __construct(EntityManagerInterface $entityManager, MessageBusInterface $bus)
+    public function __construct(EntityManagerInterface $entityManager, MessageBusInterface $bus, LoggerInterface $logger)
     {
+        $logger->error('Zashel');
         parent::__construct(self::$defaultName);
         $this->entityManager = $entityManager;
         $this->bus = $bus;
@@ -25,22 +27,23 @@ class SendNotificationsCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $now = new \DateTime();
-        $notifications = $this->entityManager->getRepository(TaskNotification::class)
-            ->findBy(['triggerTime' => $now]);
-        foreach ($notifications as $notification) {
-            $this->bus->dispatch(new TaskNotificationMessage(
-                $notification->message,
-                $notification->notificationType,
-                $notification->task->id,
-                $notification->triggerTime
-            ));
-
-            $notification->isSent = true;
-            $this->entityManager->persist($notification);
-        }
-
-        $this->entityManager->flush();
+        $output->writeln('Running the task every minute.');
+//        $now = new \DateTime();
+//        $notifications = $this->entityManager->getRepository(TaskNotification::class)
+//            ->findBy(['triggerTime' => $now]);
+//        foreach ($notifications as $notification) {
+//            $this->bus->dispatch(new TaskNotificationMessage(
+//                $notification->message,
+//                $notification->notificationType,
+//                $notification->task->id,
+//                $notification->triggerTime
+//            ));
+//
+//            $notification->isSent = true;
+//            $this->entityManager->persist($notification);
+//        }
+//
+//        $this->entityManager->flush();
 
         return Command::SUCCESS;
     }
--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -6,7 +6,6 @@ use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
